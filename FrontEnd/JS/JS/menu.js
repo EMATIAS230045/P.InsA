b9a0efd8a9578f846ce5130a65a9a344c9cf1596
Citlalli Pérez Dionicio <230237@utxicotepec.edu.mx>
2024-08-04 23:56:11 -0600

    Carpeta JS
    Se subió la carpeta de JavaScript.

---
+++ FrontEnd/JS/JS/menu.js
@@ -0,0 +1,29 @@
+// Obtener el botón y la pantalla emergente
+const boton = document.getElementById('dos');
+
+
+// Agregar un evento de clic al botón
+boton.addEventListener('click', () => {
+    // Redirigir a la nueva página
+    window.location.href = 'login.html';
+});
+
+// Obtener el botón y la pantalla emergente
+const boton2 = document.getElementById('tres');
+
+
+// Agregar un evento de clic al botón
+boton2.addEventListener('click', () => {
+    // Redirigir a la nueva página
+    window.location.href = 'perfil.html';
+});
+
+// Obtener el botón y la pantalla emergente
+const boton3 = document.getElementById('cuatro');
+
+
+// Agregar un evento de clic al botón
+boton3.addEventListener('click', () => {
+    // Redirigir a la nueva página
+    window.location.href = 'asistencia.html';
+});
